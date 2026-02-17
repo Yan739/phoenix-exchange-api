44b7c2e1dce916d3de136f1e508528fbeeed6154
Yann Cabrel NGATEU TAGNE <ngateuyann8@gmail.com>
2026-02-17 15:00:29 +0100

    initial commit

---
+++ src/main/java/com/yann/phoenix_exchange_api/PhoenixExchangeApiApplication.java
@@ -0,0 +1,13 @@
+package com.yann.phoenix_exchange_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoenixExchangeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoenixExchangeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yann/phoenix_exchange_api/PhoenixExchangeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yann.phoenix_exchange_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoenixExchangeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
